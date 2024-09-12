6b3b32eea58a1011fe9f4aaf4cdcf727e379e0fa
Jecksu <330294766@qq.com>
2024-09-13 01:11:08 +0800

    :art: optimize menu

--- my-app/src/components/menu.tsx
+++ my-app/src/components/menu.tsx
@@ -2,6 +2,7 @@
 
 import React, { CSSProperties, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import user from '../utils/user';
 
 const styles: { [key: string]: CSSProperties } = {
     menu: {
@@ -10,12 +11,27 @@ const styles: { [key: string]: CSSProperties } = {
         alignItems: 'stretch',
         position: 'absolute',
         backgroundColor: 'white',
-        width: '180px',
+        width: '150px',
         border: '1px solid #ccc',
         borderRadius: '5px',
         zIndex: 9999,
         padding: '5px',
         boxShadow: '0 2px 5px rgba(0,0,0,0.1)',
+    },
+    userinfo: {
+        padding: '4px',
+        margin: '4px 4px 8px 4px',
+        border: '1px solid transparent',
+        borderRadius: '4px',
+        textAlign: 'center',
+        color: '#333',
+        backgroundColor: '#f0f0f0',
+        fontSize: '14px',
+        fontWeight: 'bold',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        order: -1,
     },
     menuItem: {
         padding: '8px 12px',
@@ -27,12 +43,13 @@ const styles: { [key: string]: CSSProperties } = {
         fontWeight: 'normal',
         cursor: 'pointer',
         textAlign: 'left',
-        transition: 'background-color 0.2s',
+       
         marginLeft: '20px',
     },
     menuItemHover: {
         backgroundColor: '#f5f5f5',
     }
+
 }
 
 function Menu() {
@@ -68,6 +85,9 @@ function Menu() {
     return (
         <div style={styles.menu}>
             <h1>Menu</h1>
+            <div style={styles.userinfo}>
+                <p>{curuser.username}</p>
+            </div>
             <button onClick={handleUserDashboardClick} style={styles.menuItem}>profile</button>
             <button onClick={handleSettingClick} style={styles.menuItem}>setting</button>
             <button onClick={handleLogoutClick} style={styles.menuItem}>log out</button>
